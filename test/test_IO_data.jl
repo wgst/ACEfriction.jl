f2abca260618563faced96141b5c655abf5fda0d
MatthiasSachs <m.sachs@bham.ac.uk>
2024-11-02 18:47:45 +0000

    update

--- test/test_IO_data.jl
+++ test/test_IO_data.jl
@@ -8,10 +8,11 @@ using ACEbase.Testing
 fname = "/test/test-data-100"
 filename = string(pkgdir(ACEfriction),fname,".h5")
 
+filename2 = string(tempname(),".h5")
 data1 = ACEfriction.DataUtils.load_h5fdata(filename); 
-save_h5fdata(data1, "./test/test-data-temp.h5");
-data2= load_h5fdata("./test/test-data-temp.h5");
-rm("./test/test-data-temp.h5")
+save_h5fdata(data1,filename2);
+data2= load_h5fdata(filename2);
+rm(filename2)
 
 @test all([all([getfield(d1.at,f) == getfield(d2.at,f) && 
 typeof(getfield(d1.at,f)) == typeof(getfield(d2.at,f)) for f in fieldnames(typeof(d1.at))])
